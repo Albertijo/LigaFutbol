72982f2ebfa957cfc8a63c29c994da1f7093c169
Alberto Navarro <albertijo1982@gmail.com>
2020-06-11 20:47:19 +0200

    Test añadido

---
+++ src/test/java/com/sanvalero/ligafutbol/testcamiseta.java
@@ -0,0 +1,16 @@
+package com.sanvalero.ligafutbol;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testcamiseta {
+
+		@Test
+	public void test() {
+		Short camiseta1 = null;
+		Short camiseta2 = null;
+		assertEquals (camiseta1, camiseta2);
+}
+		
+}
